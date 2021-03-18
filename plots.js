6c0f8125ffd32186a79e2fc79b0a0bc541059894
Pierre-Olivier Boisvert <pob944@hotmail.com>
2021-03-17 20:54:37 -0400

    Barchart

--- plots.js
+++ plots.js
@@ -1,1 +1,12 @@
-Plotly.newPlot("plotArea", [{x: [1, 2, 3], y: [10, 20, 30]}]);
+// Init
+// Plotly.newPlot("plotArea", [{x: [1, 2, 3], y: [10, 20, 30]}]);
+
+var trace = [
+  {
+    x: ["burrito", "pizza", "chicken"],
+    y: [10, 18, 5],
+    type: "bar",
+  },
+];
+
+Plotly.newPlot("plotArea", trace);
