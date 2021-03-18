47e0725c322f19a430697657f6b66d9b76f73d9e
Pierre-Olivier Boisvert <pob944@hotmail.com>
2021-03-18 12:09:46 -0400

    Final Module

--- 12_4/plots.js
+++ 12_4/plots.js
@@ -1,35 +1,43 @@
-// Renders the initial visualization
 function init() {
-  data = [
-    {
-      x: [1, 2, 3, 4, 5],
-      y: [1, 2, 4, 8, 16],
-    },
-  ];
-  Plotly.newPlot("plot", data);
-}
+  var selector = d3.select("#selDataset");
 
-d3.selectAll("#dropdownMenu").on("change", updatePlotly);
-function updatePlotly() {
-  var dropdownMenu = d3.select("#dropdownMenu");
-  var dataset = dropdownMenu.property("value");
-
-  var xData = [1, 2, 3, 4, 5];
-  var yData = [];
-
-  if (dataset === "dataset1") {
-    yData = [1, 2, 4, 8, 16];
-  }
-
-  if (dataset === "dataset2") {
-    yData = [1, 10, 100, 1000, 10000];
-  }
-
-  var trace = {
-    x: [xData],
-    y: [yData],
-  };
-  Plotly.restyle("plot", trace);
+  d3.json("samples.json").then((data) => {
+    console.log(`The data is ${data}`);
+    // console.log(data);
+    var sampleNames = data.names;
+    sampleNames.forEach((sample) => {
+      selector.append("option").text(sample).property("value", sample);
+    });
+  });
 }
 
 init();
+
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    // Store call back
+    var { metadata } = data;
+    // Filter by id
+    var resultArray = metadata.filter((sampleObj) => sampleObj.id == sample);
+    // Return array
+    var result = resultArray[0];
+    // Display selected ID array
+    console.log(result);
+    // Pan to append
+    var PANEL = d3.select("#sample-metadata");
+
+    PANEL.html("");
+
+    PANEL.append("h6").text(`ID: ${result.id}`);
+    PANEL.append("h6").text(`ETHNICITY: ${result.ethnicity}`);
+    PANEL.append("h6").text(`GENDER: ${result.gender}`);
+    PANEL.append("h6").text(`LOCATION: ${result.location}`);
+    PANEL.append("h6").text(`BBTYPE: ${result.bbtype}`);
+    PANEL.append("h6").text(`WFREQ: ${result.wfreq}`);
+  });
+}
+
+function optionChanged(newSample) {
+  // console.log("changedOpt");
+  buildMetadata(newSample);
+}
